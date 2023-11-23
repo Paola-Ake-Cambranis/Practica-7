ed95ea399cadcfcc1a42fc51bba414498f0c751e
Paola-Ake-Cambranis <148833125+Paola-Ake-Cambranis@users.noreply.github.com>
2023-11-23 00:13:49 -0600

    Version 1.2

---
+++ Practicaa7/src/main/java/paola/practicaa7/Practicaa7.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package paola.practicaa7;
+
+/**
+ *
+ * @author VALPAO
+ */
+public class Practicaa7 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
