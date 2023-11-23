8110165022201b2420e99296bd3aef5b470d4c2e
Paola-Ake-Cambranis <148833125+Paola-Ake-Cambranis@users.noreply.github.com>
2023-11-23 00:39:30 -0600

    version 1

---
+++ Practiica7/Controladora.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.io.*;
+
+public class Controladora {
+public static void main (String args[]){
+try{
+WebMaker generador = new WebMaker();
+PrintWriter escritor = new PrintWriter("index.html");
+Scanner lector = new Scanner(new FileReader("form.txt"));
+escritor.println(generador.imprimirEncabezado());
+escritor.println(generador.imprimirTexto("Una Gwen chikita trabajando"));
+escritor.println(generador.insertarImagen("trabajando.jpg"));
+while(lector.hasNext()) {
+escritor.println(lector.nextLine());
+}
+escritor.println(generador.imprimirCierre());
+lector.close();
+escritor.close();
+}
+catch (FileNotFoundException e){
+}
+}
+}
