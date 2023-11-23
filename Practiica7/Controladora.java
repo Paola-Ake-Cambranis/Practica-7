16bae3c75a1db278fa7983f1cc87bf6cf8e13c5e
Paola-Ake-Cambranis <148833125+Paola-Ake-Cambranis@users.noreply.github.com>
2023-11-23 01:14:23 -0600

    Version 1.2

--- Practiica7/Controladora.java
+++ Practiica7/Controladora.java
@@ -1,36 +1,25 @@
 import java.util.Scanner;
 import java.io.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class Controladora {
 public static void main (String args[]){
-/*try{
+try{
 WebMaker generador = new WebMaker();
 PrintWriter escritor = new PrintWriter("index.html");
 Scanner lector = new Scanner(new FileReader("form.txt"));
-@@ -27,6 +29,24 @@ public static void main (String args[]){
+escritor.println(generador.imprimirEncabezado());
+escritor.println(generador.imprimirTexto("Hachi!"));
+escritor.println(generador.insertarImagen("Hachi.jpg"));
+escritor.println(generador.imprimirTexto("Nana!"));
+escritor.println(generador.insertarImagen("Nana.jpg"));
+while(lector.hasNext()) {
+escritor.println(lector.nextLine());
+}
+escritor.println(generador.imprimirCierre());
+lector.close();
 escritor.close();
 }
 catch (FileNotFoundException e){
-}*/
-try{
-DataOutputStream escritor = new DataOutputStream (new FileOutputStream("hola.bin"));
-
-
-escritor.writeInt(33465);
-escritor.writeChar('a');
-escritor.writeChar('z');
-escritor.writeShort(10);
-escritor.close();
-DataInputStream lector = new DataInputStream (new FileInputStream("hola.bin"));
-System.out.println(lector.readInt());
-System.out.println(lector.readChar());
-System.out.println(lector.readChar());
-System.out.println(lector.readShort());
-escritor.close();
 }
-catch (FileNotFoundException e){} 
-catch (IOException ex) {}
 }
 }
