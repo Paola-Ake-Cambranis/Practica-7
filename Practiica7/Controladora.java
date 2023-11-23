a83e485f2c982dab878ac06e32adb15ae3ab9bef
Paola-Ake-Cambranis <148833125+Paola-Ake-Cambranis@users.noreply.github.com>
2023-11-23 01:27:37 -0600

    version 1.4

--- Practiica7/Controladora.java
+++ Practiica7/Controladora.java
@@ -1,25 +1,23 @@
 import java.util.Scanner;
 import java.io.*;
 
+/**
+ *
+ * @author nenod
+ */
 public class Controladora {
 public static void main (String args[]){
 try{
-WebMaker generador = new WebMaker();
-PrintWriter escritor = new PrintWriter("index.html");
-Scanner lector = new Scanner(new FileReader("form.txt"));
-escritor.println(generador.imprimirEncabezado());
-escritor.println(generador.imprimirTexto("Hachi!"));
-escritor.println(generador.insertarImagen("Hachi.jpg"));
-escritor.println(generador.imprimirTexto("Nana!"));
-escritor.println(generador.insertarImagen("Nana.jpg"));
-while(lector.hasNext()) {
-escritor.println(lector.nextLine());
-}
-escritor.println(generador.imprimirCierre());
+ObjectInputStream
+lector = new ObjectInputStream(new FileInputStream("web.obj"));
+PrintWriter escritor = new PrintWriter("main.html");
+WebMaker pagina = (WebMaker)(lector.readObject());
+escritor.print(pagina.printPage());
 lector.close();
 escritor.close();
 }
-catch (FileNotFoundException e){
-}
+catch (FileNotFoundException e){}
+catch(IOException e) {}
+catch(ClassNotFoundException e){}
 }
 }
